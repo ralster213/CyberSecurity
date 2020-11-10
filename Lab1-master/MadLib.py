b8fc03edf20741ae291765bb3d07d37a94ca21a4
ralster213 <raleigh.page2@gmail.com>
2020-11-10 13:17:52 -0600

    All Done

--- Lab1-master/MadLib.py
+++ Lab1-master/MadLib.py
@@ -5,8 +5,13 @@
 
 def main():
     #Ask user for words
-
+    NounList = input("Choose three of your favorite Nouns and type them in seperately: ")
+    NounList = NounList.split()
+    print(NounList)
+    VerbList = input("Now pick three of your most cherished Verbs and type those in seperately: ")
+    VerbList = VerbList.split()
+    print(VerbList)
     #Print the story with the user supplied words.
-
+    print("The " + NounList[0] + " loves to " + VerbList[0] + " and " + VerbList[1] + " " + NounList[1] + " but especially " + VerbList[2] + " " + NounList[2])
 
 main()
