35495333d0efae145db66ae88c58d25a962ab5bc
ralster213 <raleigh.page2@gmail.com>
2020-11-12 11:35:15 -0600

    All Done

--- Lab3-master/TempConvert.py
+++ Lab3-master/TempConvert.py
@@ -3,10 +3,15 @@
 #Date:
 #Assignment:
 
+from time import sleep
 
 def main():
     #Prompt the user for a Fahrenheit temperature
+    (tempF) = input("Type in a temperature in degrees Fahrenheit: ")
     #Convert that temperature to celsius, rounding to 1 decimal percision
+    tempC = ((float(tempF)-32) *5/9 )
+    sleep(1)
+    print(round(tempC, 1))
     #Output converted temperature.
 
 main()
