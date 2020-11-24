61107b90c0c47f12d9f30b6f95254f62631ce500
ralster213 <raleigh.page2@gmail.com>
2020-11-23 20:25:44 -0600

    Finished WordCount

--- Lab6-master/WordCount.py
+++ Lab6-master/WordCount.py
@@ -4,10 +4,23 @@
 #Assignment:
 
 def main():
-    textFile = open("gettysberg.txt", 'r')
+    textFile = open("/Users/90305007/Developer/CyberSecurity/Lab6-master/gettysberg.txt", 'r')
+    #count = 0
+    #for line in textFile:
+    #    currentLine = line
+    #    for word in currentLine:
+    #        if " " in word:
+    #            count += 1
 
-    for line in textFile:
-        print(line)
-
+        #print(line)
+    #print(count)
+    words = []    
+    numWords = 0    
+    for line in textFile:        
+        words.append(line.split())    
+    for line in words:        
+        for word in line:            
+            numWords = numWords + 1    
+            print(numWords)
 
 main()
